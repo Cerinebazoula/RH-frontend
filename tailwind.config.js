9a4fa9d0960610fcac80a44326fd72c54285c0c6
Moncef SAMAI <moncef.samai.rol2@gmail.com>
2023-01-07 12:25:12 +0100

    adding tailwindcss & prettier

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      // put here your custom theme (colors, screens, etc.)
+    },
+  },
+  plugins: [
+    // put here your custom plugins (typography, forms, etc.)
+  ],
+};
