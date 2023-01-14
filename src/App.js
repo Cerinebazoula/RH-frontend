042748d2a1489076c678bb7d8dd208135c8cbf78
Mounir <lm_melzi@esi.dz>
2023-01-14 11:46:29 +0100

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center w-[100vw] h-[100vh] text-6xl">
+      404 - Page Not Found
+    </div>
+  );
+}
